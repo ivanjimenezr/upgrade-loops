e05a92751a1078eb45f45de11d6e8174c8fc73c3
Ivan Jimenez <chiklete@gmail.com>
2021-10-20 15:12:18 +0200

    Iteration#3

---
+++ iteracion3.js
@@ -0,0 +1,5 @@
+//Iteración #3: Probando For...of
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+for (value of placesToTravel) {
+    console.log(value)
+}
